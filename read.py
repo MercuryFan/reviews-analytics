c34fc9077af4879fd5424a8640d17d8da63424a0
Scott Fan <tea79649@hotmail.com>
2022-07-21 17:23:43 +0800

    calc_avg len(reviews)

--- read.py
+++ read.py
@@ -6,7 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1            #count = count + 1
 		if count % 1000 == 0: #%為求餘數的意思,ex:7除3 = 2..1 取餘數就是1
 			print(len(data))  #這個寫法等於每一千筆才印一次數量。
-print(len(data)) #印出總共幾筆資料
-print(data[0])
-print('----------------')
-print(data[1])
+print('檔案讀取完了,總共有:', len(data), '筆資料。') #印出總共幾筆資料
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)  #sum_len = sum_len + len(d)
+
+print('每筆留言平均長度是:', sum_len/len(data)) #留言總長度(sum_len) 除以 留言總筆數len(data)
